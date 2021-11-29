b46b54bcca7f6398876cfe193a306d9d03662105
Danilo <dcassianodias@gmail.com>
2021-11-29 17:51:41 -0300

    Inclusão de Cadastro Professor

--- src/main/java/br/com/primeiroprojetospring/controller/ProfessorController.java
+++ src/main/java/br/com/primeiroprojetospring/controller/ProfessorController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.primeiroprojetospring.domain.Professor;
 import br.com.primeiroprojetospring.service.ProfessorService;
 
 @Controller
@@ -22,4 +24,18 @@ public class ProfessorController {
 		mView.addObject("professores", professorService.buscarTodosProfessores());
 		return mView;
 	}
+	
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarProfessor() {
+		ModelAndView mView = new ModelAndView("professor/cadastraProfessor");
+		mView.addObject("professor", new Professor());
+		return mView;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarProfessor(Professor professor) {
+		professorService.salvar(professor);
+		return listaTodosProfessores();
+	}
 }
+
